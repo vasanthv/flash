3fd91fbc83730f3029cb0beeea1bf0f88aeb0324
Vasanth.V <imvasanthv@gmail.com>
2022-05-20 22:16:48 +0530

    Set `sw.js` version

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
  * Flash Service worker
  */
 
-const currentCacheName = "flash-v-~VERSION";
+const currentCacheName = "flash-v-~1.0.0";
 
 self.addEventListener("install", function(e) {
 	console.log("Install event triggered. New updates available.");
