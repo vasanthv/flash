b4b020c9f6b109855e819ac04bdff583eff98d94
Vasanth.V <imvasanthv@gmail.com>
2022-05-21 15:29:36 +0530

    Added banner image

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
  * Flash Service worker
  */
 
-const currentCacheName = "flash-v-~1.0.4";
+const currentCacheName = "flash-v-~1.0.5";
 
 self.addEventListener("install", function(e) {
 	console.log("Install event triggered. New updates available.");
